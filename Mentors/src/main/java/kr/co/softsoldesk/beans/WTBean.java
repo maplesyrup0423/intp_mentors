87612da751ea7fc222b0109e18bef44fb1dbe2b7
maplesyrup0423 <105406712+maplesyrup0423@users.noreply.github.com>
2024-02-29 10:05:35 +0900

    연수 Bean파일 추가

---
+++ Mentors/src/main/java/kr/co/softsoldesk/beans/WTBean.java
@@ -0,0 +1,23 @@
+package kr.co.softsoldesk.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WTBean {
+	private String WT_Key;
+	private String WT_Title;
+	private int WT_Price;
+	private int WT_TrainingTime;
+	private int WT_Tag_School; //1 초등학교 | 2 중학교 3 | 고등학교
+	private int WT_Tag_Time; //1 0~60분 | 2 60~90분 | 3 90분이상
+	private int WT_Tag_TypeCategory;// 1 교과지도 | 2 학습지도 | 3 생활지도
+	private String WT_Thumbnail;
+	private String WT_Info;
+	private String WT_Text;
+	private String WT_Video;
+	
+	//----------------------------------------------------
+	
+}
