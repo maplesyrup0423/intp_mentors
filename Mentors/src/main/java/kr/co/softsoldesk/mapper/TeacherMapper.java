f0140eee520b47fc3751d64b1fb429706c48658a
maplesyrup0423 <105406712+maplesyrup0423@users.noreply.github.com>
2024-03-02 01:01:31 +0900

    TeacherMapper 의 getLoginTeacherInfo 수정
    받아오는 값의 teacher_id 추가
    teacher_id 소문자로 변경

--- Mentors/src/main/java/kr/co/softsoldesk/mapper/TeacherMapper.java
+++ Mentors/src/main/java/kr/co/softsoldesk/mapper/TeacherMapper.java
@@ -7,10 +7,10 @@ import kr.co.softsoldesk.beans.TeacherBean;
 
 public interface TeacherMapper {
 	//로그인시 유저 정보 받기
-	@Select("select Teacher_Name, Teacher_Birth, Teacher_Gender, " 
+	@Select("select teacher_id, Teacher_Name, Teacher_Birth, Teacher_Gender, " 
 			+ "Teacher_Email, Teacher_Tel, Teacher_Nickname, "
 			+ "Teacher_Work_In, Teacher_Type, Teacher_Neis "
-			+ "from Teacher_Info where Teacher_Id = #{Teacher_Id} and Teacher_Password=#{Teacher_Password}")
+			+ "from Teacher_Info where teacher_id = #{teacher_id} and Teacher_Password=#{Teacher_Password}")
 	TeacherBean getLoginTeacherInfo(TeacherBean teacherBean);
 	
 	//아이디 중복체크
