cbfce3df29cb6b821cba2ca03a19aeae2a3244b5
maplesyrup0423 <105406712+maplesyrup0423@users.noreply.github.com>
2024-03-06 12:24:49 +0900

    wtt Mapper sql문 공백

--- Mentors/src/main/java/kr/co/softsoldesk/mapper/WTT_Mapper.java
+++ Mentors/src/main/java/kr/co/softsoldesk/mapper/WTT_Mapper.java
@@ -15,7 +15,7 @@ public interface WTT_Mapper {
 	List<WTT_Bean> addMyRoomAll(String teacher_id);
 	
 	//WT_my_room 정보 필터
-	@Select("select wtt.wtt_Key,wtt.WTT_Completion, wtt.wtt_payment_date, wt.wt_Title, wt.wt_TrainingTime, wt.wt_Tag_School, wt.wt_Tag_TypeCategory, wtt.WTT_viewing_time"
+	@Select("select wtt.wtt_Key,wtt.WTT_Completion, wtt.wtt_payment_date, wt.wt_Title, wt.wt_TrainingTime, wt.wt_Tag_School, wt.wt_Tag_TypeCategory, wtt.WTT_viewing_time "
 			+ "from workplace_t wt inner join wtt on wtt.WT_key=wt.WT_key and wtt.WTT_Completion=#{WTT_Completion} inner join teacher_info t on wtt.teacher_id = t.teacher_id and wtt.teacher_id=#{teacher_id}")
 	List<WTT_Bean> addMyRoomSelect(@Param("WTT_Completion") int WTT_Completion, @Param("teacher_id")String teacher_id);
 	
