6733b2af94e37500882f7e1437df159bb5af1213
maplesyrup0423 <105406712+maplesyrup0423@users.noreply.github.com>
2024-03-06 17:13:08 +0900

    북 메퍼에 카테고리별 리스트 정보 받기 쿼리문 생성

--- Mentors/src/main/java/kr/co/softsoldesk/mapper/BookMapper.java
+++ Mentors/src/main/java/kr/co/softsoldesk/mapper/BookMapper.java
@@ -13,9 +13,12 @@ public interface BookMapper {
 	List<BookBean> getBookList();
 	
 	//북 디테일 정보 받아오기
-	@Select("select bk_Key,bk_img, bk_title,bk_autor,bk_date,bk_isbn,bk_publisher,bk_price,bk_yes24,bk_ald,_kobo "
+	@Select("select bk_Key,bk_img, bk_title,bk_autor,bk_date,bk_isbn,bk_publisher,bk_price,bk_yes24,bk_ald,bk_kobo "
 			+ ",bk_info, bk_content,bk_autorinfo,bk_category from booklist where bk_Key=#{bk_Key}")
 	BookBean getBookInfo(String bk_Key);
 	
+	//카테고리별 북리스트 받아오기
+	@Select("select BK_Key,BK_img,BK_title,BK_autor,BK_date,BK_isbn,BK_price from booklist where BK_category=#{bk_category}")
+	List<BookBean> getBookListSelect(int bk_category);
 
 }
